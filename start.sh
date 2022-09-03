4c0fe1966c30156b0f114a2183ba64cc6f8f0b42
Luzian Trapletti <luzian@trapletti.org>
2022-09-03 14:07:57 +0000

    improved vesion

--- start.sh
+++ start.sh
@@ -1,3 +1,12 @@
-source /home/luzian/miniconda3/etc/profile.d/conda.sh
-conda activate boolq
-gunicorn src.main:server -b 127.0.0.1:8050 >& log.txt &
+#!/bin/bash
+ps cax | grep gunicorn > /dev/null
+now=`date`
+if [ $? -eq 0 ]; then
+  echo "$now - gunicorn is running"
+else
+  echo "$now - gunicorn is not running, starting"
+  cd /home/luzian/Boolq
+  source /home/luzian/miniconda3/etc/profile.d/conda.sh
+  conda activate boolq
+  gunicorn src.main:server -b 127.0.0.1:8050 >& log.txt &
+fi
