e0902ef94e7e3738960b9da18ba4a606ad4c3afc
Luzian Trapletti <luzian@trapletti.org>
2022-09-03 14:16:27 +0000

    fix bug

--- start.sh
+++ start.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 ps cax | grep gunicorn > /dev/null
-now=`date`
 if [ $? -eq 0 ]; then
+  now=`date`
   echo "$now - gunicorn is running"
 else
+  now=`date`
   echo "$now - gunicorn is not running, starting"
   cd /home/luzian/Boolq
   source /home/luzian/miniconda3/etc/profile.d/conda.sh
