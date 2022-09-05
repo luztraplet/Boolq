fe205629aa096817d741b97f4f63ffe9afff979d
Luzian Trapletti <luzian@trapletti.org>
2022-09-05 21:20:42 +0200

    num workers changed

--- start.sh
+++ start.sh
@@ -9,5 +9,5 @@ else
   cd /home/luzian/Boolq
   source /home/luzian/miniconda3/etc/profile.d/conda.sh
   conda activate boolq
-  gunicorn src.main:server -b 127.0.0.1:8050 >& log.txt &
+  gunicorn src.main:server -w 4 -b 127.0.0.1:8050 >& log.txt &
 fi
